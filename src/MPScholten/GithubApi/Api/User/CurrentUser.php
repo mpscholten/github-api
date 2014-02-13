1ec4f64f5fde9d354fc9a5fc9da2a308e8e7522e
Marc Philip Scholten <marc@pedigital.de>
2014-02-13 19:33:06 +0100

    Added some documentation

--- src/MPScholten/GithubApi/Api/User/CurrentUser.php
+++ src/MPScholten/GithubApi/Api/User/CurrentUser.php
@@ -7,6 +7,12 @@ namespace MPScholten\GithubApi\Api\User;
 use  Guzzle\Http\Client;
 use MPScholten\GithubApi\Api\Repository\Repository;
 
+/**
+ * This class is mostly the same as User, the only difference is that it also loads some
+ * private data (e.g. private repositories) if you're allowed to do so.
+ *
+ * @link http://developer.github.com/v3/users/#update-the-authenticated-user
+ */
 class CurrentUser extends User
 {
     // relations
@@ -19,17 +25,30 @@ class CurrentUser extends User
         $this->load();
     }
 
+    /**
+     * @link http://developer.github.com/v3/users/#get-the-authenticated-user
+     *
+     * Loads the current authenticated user
+     */
     protected function load()
     {
         $this->populate($this->get('user'));
     }
 
     /**
-     * @param string $type
+     * @link http://developer.github.com/v3/repos/#list-your-repositories
+     *
+     * @param string $type Can be one of all, owner, public, private, member. Default: all
+     * @throws \InvalidArgumentException In case the $type is not valid
      * @return Repository[]
      */
     public function getRepositories($type = 'all')
     {
+        $validTypes = ['all', 'owner', 'public', 'private', 'member'];
+        if(!in_array($type, $validTypes)) {
+            throw new \InvalidArgumentException(sprintf('Invalid type, expected one of "%s"', implode(', ', $validTypes)));
+        }
+
         if (!isset($this->repositories[$type])) {
             $this->repositories[$type] = $this->loadRepositories($type);
         }
--- src/MPScholten/GithubApi/Api/User/User.php
+++ src/MPScholten/GithubApi/Api/User/User.php
@@ -9,6 +9,9 @@ use MPScholten\GithubApi\Api\Organization\Organization;
 use MPScholten\GithubApi\TemplateUrlGenerator;
 use MPScholten\GithubApi\UrlType;
 
+/**
+ * @link http://developer.github.com/v3/users/
+ */
 class User extends AbstractApi
 {
     // relations
@@ -41,26 +44,44 @@ class User extends AbstractApi
         $this->htmlUrl = $data['html_url'];
     }
 
+    /**
+     * @return string The login name of the github account, e.g. "octocat"
+     */
     public function getLogin()
     {
         return $this->login;
     }
 
+    /**
+     * @return string The avatar url, e.g. "https://github.com/images/error/octocat_happy.gif"
+     */
     public function getAvatarUrl()
     {
         return $this->avatarUrl;
     }
 
+    /**
+     * @return string The gravatar id
+     * @link https://gravatar.com/
+     */
     public function getGravatarId()
     {
         return $this->gravatarId;
     }
 
+    /**
+     * @return integer The user id, e.g. 42
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * @param string $type Can be 'html' or 'api'
+     * @return string The url, e.g. https://github.com/octocat (if $type is html)
+     * @throws \InvalidArgumentException
+     */
     public function getUrl($type = 'html')
     {
         switch ($type) {
@@ -73,13 +94,19 @@ class User extends AbstractApi
         throw new \InvalidArgumentException(sprintf('Invalid url type "%s", expected one of "%s"', $type, implode(', ', ['html', 'api'])));
     }
 
+    /**
+     * @return string The real name of the user, e.g. "monalisa octocat"
+     */
     public function getName()
     {
         return $this->name;
     }
 
     /**
-     * @return Organization[]
+     * @link http://developer.github.com/v3/orgs/#list-user-organizations
+     * "List all public organizations for an unauthenticated user. Lists private and public organizations for authenticated users."
+     *
+     * @return Organization[] The public organizations the user is member of
      */
     public function getOrganizations()
     {
