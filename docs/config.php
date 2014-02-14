7bbf59f125d94d4de07c0128e194803cb042feb6
Marc Philip Scholten <marc@pedigital.de>
2014-02-14 21:06:41 +0100

    added sami api docs

---
+++ docs/config.php
@@ -0,0 +1,19 @@
+<?php
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('Tests')
+    ->in(__DIR__.'/../src/MPScholten/GithubApi/')
+;
+
+return new Sami($iterator, [
+    'title' => 'Github Api',
+    'theme' => 'default',
+    'build_dir' => __DIR__.'/api/',
+    'cache_dir' => __DIR__.'/api/cache',
+    'default_opened_level' => 2
+]);
